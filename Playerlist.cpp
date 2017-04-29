9b1b2ea73dff750cf293973adbf1ceb6c6d4f4f3
Enoch Ng <ewnoch@gmail.com>
2017-04-29 04:42:17 -0500

    Fix syntax

--- Playerlist.cpp
+++ Playerlist.cpp
@@ -4,7 +4,7 @@
 int Playerlist::add(std::string userid, int state) {
 	// Check to see if the player is already somehow there
 	for (unsigned int i = 0; i < players.size(); i++) {
-		if (players[i].get_id() == userid) {
+		if (players[i].userid == userid) {
 			return -1;
 		}
 	}
